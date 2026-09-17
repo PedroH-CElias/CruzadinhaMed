de8af585436c914d45b9cabbc4dcd49450fb1eca
PedroH-CElias <phenriqueelias29@gmail.com>
2026-09-17 15:54:50 -0300

    Reajuste local front e criação backend

---
+++ backend/med/src/main/java/com/cruzadinha/med/MedApplication.java
@@ -0,0 +1,13 @@
+package com.cruzadinha.med;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedApplication.class, args);
+	}
+
+}
